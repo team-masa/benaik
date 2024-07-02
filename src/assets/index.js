355df9a1bfec4ffa6c349a25be71c85bf7e75df6
Mabel Aikins Acquah <acquahmabel60@gmail.com>
2024-07-02 12:25:45 +0000

    New Styles

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,13 @@
-import weddOne from "./images/wedd.jpg"
+import weddOne from "./images/wedd.jpg";
+import weddTwo from "./images/wedd1.jpg";
+import partyOne from "./images/party.jpg"; 
+import partyTwo from "./images/party1.jpg";
+
+
+
+
+
+export {weddOne};
+export {weddTwo};
+export {partyOne};
+export {partyTwo};
--- src/pages/landing/components/hero.jsx
+++ src/pages/landing/components/hero.jsx
@@ -1,9 +1,33 @@
+import { partyOne } from "../../../assets"
 
 
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
+const hero = () => {
+    return (
+        <div className="">
+
+            <div className="bg-cover">
+         <span className="font-bold">Join Us and Discover More</span>
+                </div>
+            
+
+            <div className="w-full">
+                <img src={partyOne} alt="Hero background" className="object-cover w-full h-full" />
+            </div>
+            <div className="flex justify-center px-6 py-3 rounded-full border-2 border-black">
+                <button>LOG IN</button>
+                </div>
+                
+                
+                
+
+                <div> <button>SIGN UP</button>
+
+                </div>
+               
+            
+
+        </div>
+    )
 }
 
-export default Hero
+export default hero
