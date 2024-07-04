8cd383f3a6c7c8a8c3cef6195545a371881cadc8
Mabel Aikins Acquah <acquahmabel60@gmail.com>
2024-07-04 14:31:50 +0000

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,11 @@ import Parties from "./pages/parties";
 import WeddingPage from "./pages/weddings";
 import About from "./pages/about-us";
 import ContactUs from "./pages/contact-us";
-<<<<<<< HEAD
 import Funeral from "./pages/funerals";
-=======
 import Events from "./pages/events";
 import SignUpForm from "./pages/signup";
 import LoginForm from "./pages/login";
 import EventForm from "./pages/eventForms/components/createEvents";
->>>>>>> e4dd718691df067cb585fd8b8c30ba7e6b5124fc
 
 function App() {
   const router = createBrowserRouter([
@@ -41,20 +38,12 @@ function App() {
         },
 
         {
-<<<<<<< HEAD
-          path: "event/parties",
-=======
           path: "events/parties",
->>>>>>> e4dd718691df067cb585fd8b8c30ba7e6b5124fc
           element: <Parties />
         },
 
         {
-<<<<<<< HEAD
-          path: "event/wedding",
-=======
           path: "events/weddings",
->>>>>>> e4dd718691df067cb585fd8b8c30ba7e6b5124fc
           element: <WeddingPage />
         },
         {
@@ -63,7 +52,7 @@ function App() {
 
         },
         {
-          path: "event/funeral",
+          path: "events/funerals",
           element: <Funeral />
 
         },
